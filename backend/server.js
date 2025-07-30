312b1ab0ec2db8f913f7ad9c033792f6c0de2cb9
lucas <jinyaoj96@hotmail.com>
2025-07-30 13:44:01 +0800

    second time update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('./data-source');
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
